bb9aa6582fee7efd548cb3125a749baefd5fa5b3
Elias <edemoraesneto@us.ci.org>
2025-06-20 16:23:33 -0300

    day 11 - Eloquent relationships

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use Database\Factories\EmployerFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employer extends Model
 {
-    /** @use HasFactory<\Database\Factories\EmployerFactory> */
+    /** @use HasFactory<EmployerFactory> */
     use HasFactory;
+
+    public function jobs()
+        {
+        return $this->hasMany(Job::class);
+    }
 }
