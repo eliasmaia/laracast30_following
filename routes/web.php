a11051f6bef56922002c348e0cebd4a148eba7f1
Elias <eeliasmaia@gmail.com>
2025-07-02 16:45:00 -0300

    Finish day 18 - Editing, Updating, Deleting a resource

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+//Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
     return view('jobs.index',  [
@@ -14,10 +15,12 @@ Route::get('/jobs', function () {
     ]);
 });
 
+//Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+//Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     return view('jobs.show', [
@@ -25,6 +28,7 @@ Route::get('/jobs/{id}', function ($id) {
     ]);
 });
 
+//Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min: 3'],
@@ -36,6 +40,42 @@ Route::post('/jobs', function () {
         'salary' => request('salary'),
         'employer_id' => 1,
     ]);
+
+    return redirect('/jobs');
+});
+
+//Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', [
+        'job' =>$job,
+    ]);
+});
+
+//Update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min: 3'],
+        'salary' => ['required'],
+    ]);
+    //authorize (on hold...)
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+//Delete
+Route::delete('/jobs/{id}', function ($id) {
+    //authorize (on hold...)
+
+    $job = Job::findOrFail($id);
+    $job->delete();
 
     return redirect('/jobs');
 });
