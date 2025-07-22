b951f1c3b56398420660ed61009dc98c5b9e64ad
Elias <eeliasmaia@gmail.com>
2025-07-22 16:37:32 -0300

    Laracast 26

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home Page</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-
+    @vite(['resources/js/app.js'])
 </head>
 <body class="h-full">
     <div class="min-h-full">
